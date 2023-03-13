3d2d5f68307ba5ea5ca0eeee578e664f78727feb
oussama <zhdosm@gmail.com>
2023-03-13 04:31:35 +0100

    Feat ex03 now is finished

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -1,17 +1,14 @@
-
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string name)
-{
+HumanB::HumanB(std::string name){
 	_name = name;
 	_Bweapon = NULL;
 };
 
-void HumanB::setWeapon(Weapon *bw)
-{
+void HumanB::setWeapon(Weapon *bw){
 	_Bweapon = bw;
 }
-void HumanB::attack()
-{
-	std::cout<<_name<<" :  attacks with their "<<_Bweapon->getType()<<std::endl;
+void HumanB::attack(){
+	if (_Bweapon)
+		std::cout<<_name<<" :  attacks with their "<<_Bweapon->getType()<<std::endl;
 }
