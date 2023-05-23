c31ab039838c51077b70c11bf168a25d77e5e3ca
wassimelhamdaoui <wassim.elhamdaoui@uit.ac.ma>
2023-05-23 17:03:13 +0100

    Chore: checking is digit

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,6 +1,28 @@
 #include "ScalarConverte.hpp"
 
-int main(){
+bool is_it_digits(std::string str)
+{
 
+    for(size_t i = 0; i < str.length(); i++){
+        if (!isdigit(str[i]))
+            return (false);
+    }
+    return (true);
+}
 
+int main(int ac, char **av){
+
+    std::string str;
+
+    str = av[1];
+    if (ac  == 1 )
+        return (std::cout << "Invalid Arguments "<<std::endl, 1);
+    if (is_it_digits(str))
+    {
+        
+    }
+    else if (str.find('.')){
+
+    }
+    else if ()
 }
