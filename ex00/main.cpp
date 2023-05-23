9f0c58506faeb104caf84984b5e908261d81df68
wassimelhamdaoui <zhdosm@gmail.com>
2023-05-24 00:02:06 +0100

    Feat: print char with my own print char function

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,6 +1,13 @@
 #include "ScalarConverte.hpp"
 #include <sstream>
 
+void print_char(char c){
+	if (c <= 32)
+		std::cout<<" NON PRINTABLE CHARACTER "<<std::endl;
+	else
+		std::cout<<c<<std::endl;
+}
+
 
 bool is_it_digits(std::string str)
 {
@@ -29,6 +36,17 @@ bool is_double(std::string str){
 	return (true);
 }
 
+bool is_char(std::string str){
+
+	if(str.length() == 1 && isalpha(str[0]))
+		return (true);
+	return (false);
+}
+
+int is_valid(std::string str){
+	
+}
+
 int main(int ac, char **av){
 
     std::string str;
@@ -42,13 +60,16 @@ int main(int ac, char **av){
     str = av[1];
     if (is_it_digits(str))
     {
-		i = std::atoi(av[1]);
+		std::stringstream ss;
+		 
+		ss << str;
+		ss >> i;
         d = static_cast<double>(i);
         f = static_cast<float>(i);
         c = static_cast<char>(i);
 		std::cout<<d<<std::endl;
 		std::cout<<f<<std::endl;
-		std::cout<<c<<std::endl;
+		print_char(c);
     }
 
     else if (is_double(str)){
@@ -58,8 +79,19 @@ int main(int ac, char **av){
 		i = static_cast<int>(d);
 		f = static_cast<float>(d);
 		c = static_cast<char>(d);
+		std::cout<<i<<std::endl;
+		std::cout<<f<<std::endl;
+		print_char(c);
+    }
+	else if (is_char(str)){
+		std::stringstream ss;
+		ss << str;
+		ss >> c;
+		i = static_cast<int>(c);
+		f = static_cast<float>(c);
+		d = static_cast<double>(c);
 		std::cout<<d<<std::endl;
 		std::cout<<f<<std::endl;
-		std::cout<<c<<std::endl;
-    }
+		std::cout<<i<<std::endl;
+	}
 }
