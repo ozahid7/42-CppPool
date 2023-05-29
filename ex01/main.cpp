cd456ff99062c294aa48edc76c689f5cf5d4cb44
oussama <zhdosm@gmail.com>
2023-05-29 01:53:58 +0100

    Feat: adding main to ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,22 +1,36 @@
 #include "Serializer.hpp"
 #include <iostream>
+#include <iomanip>
+#include <sstream>
+
+
 
 int main (){
 	Data *ptr = new Data;
-
+	std::stringstream ss;
+	Data *ptr2 = new Data;
 	ptr->i = 9;
 	uintptr_t u = 4;
-	double h = 10.04;
 
-	u = reinterpret_cast<uintptr_t>(&h);
-	std::cout<<&h<<std::endl;
-	std::cout<<u<<std::endl;
-	// std::cout<<ptr<<std::endl;
+	u = Serializer::serialize(ptr);
+	std::cout<<" value of u after serialize " <<u<<std::endl; 
+	ss << std::hex << u;
+	std::cout<<" value of u after serialize  in hexadecimal "<<ss.str()<<std::endl; 
+	std::cout<<"ptr value before deserialize "<<ptr<<std::endl;
+	ptr = Serializer::deserialize(u);
+	std::cout<<"ptr value after deserialize "<<ptr<<std::endl;
+	std::cout<<ptr<<std::endl;
+	std::cout<<"ptr2 before reintrpret cast "<<ptr2<<std::endl;
+	ptr2 = reinterpret_cast<Data *>(ptr);
+	std::cout<<"ptr2 after reintrpret cast "<<ptr2<<std::endl;
+	std::cout<<ptr->i<<std::endl;
 
-	// std::cout<<ptr<<std::endl;
-	// u = Serializer::serialize(ptr);
-	// std::cout<<ptr<<std::endl;
-	// ptr = Serializer::deserialize(u);
-	// std::cout<<ptr<<std::endl;
-	// std::cout<<u<<std::endl;
+
+	//failed situations
+	int *i;
+	char *c = NULL;
+	// i = c;
+	i = reinterpret_cast<int *>(c);
+	std::cout<<"i = " <<i<<std::endl;
+
 }
